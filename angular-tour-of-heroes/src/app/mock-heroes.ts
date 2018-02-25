36eeabd9a5619d780409b36691b1a36db13aacbf
qustanley <supper615@gmail.com>
2018-02-25 21:23:01 +0800

    first commit

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero'
+
+export const HEROES:Hero[] =[
+    {id:11, name: 'Mr. 1'},
+    {id:12, name: 'Mr. 2'},
+    {id:13, name: 'Mr. 3'},
+    {id:14, name: 'Mr. 4'},
+    {id:15, name: 'Mr. 5'},
+    {id:16, name: 'Mr. 6'},
+    {id:17, name: 'Mr. 7'},
+    {id:18, name: 'Mr. 8'},
+    {id:19, name: 'Mr. 9'},
+    {id:20, name: 'Mr. 0'}
+];
